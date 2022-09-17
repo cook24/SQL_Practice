9038fdced508424cbbe16ef8dea361901ac3206d
Andy Cook <108596124+cook24@users.noreply.github.com>
2022-09-17 12:02:48 -0400

    Add files via upload

---
+++ correlated_subqueries_exercises_asmda.sql
@@ -0,0 +1,86 @@
+
+
+
+
+
+
+/*
+
+Exercise 1:
+
+Write a query that outputs all records from the Purchasing.PurchaseOrderHeader table. 
+Include the following columns from the table:
+
+    PurchaseOrderID
+    VendorID
+    OrderDate
+    TotalDue
+
+Add a derived column called NonRejectedItems which returns, 
+for each purchase order ID in the query output, 
+the number of line items from the Purchasing.PurchaseOrderDetail table 
+which did not have any rejections (i.e., RejectedQty = 0). Use a correlated subquery to do this.
+
+*/
+
+SELECT
+	poh.PurchaseOrderID,
+	poh.VendorID,
+	poh.OrderDate,
+	poh.TotalDue,
+	NonRejectedItems = 
+	(
+	SELECT 
+		COUNT(*)
+	FROM Purchasing.PurchaseOrderDetail pod
+	WHERE poh.PurchaseOrderID = pod.PurchaseOrderID
+	AND pod.RejectedQty = 0
+	)
+FROM Purchasing.PurchaseOrderHeader poh
+
+
+
+
+
+
+/*
+
+Exercise 2:
+
+Modify your query to include a second derived field called MostExpensiveItem.
+
+This field should return, for each purchase order ID, 
+the UnitPrice of the most expensive item for that order 
+in the Purchasing.PurchaseOrderDetail table.
+Use a correlated subquery to do this as well.
+
+Hint: Think of the most appropriate aggregate function to use in the correlated subquery for this scenario.
+
+*/
+
+SELECT
+	poh.PurchaseOrderID,
+	poh.VendorID,
+	poh.OrderDate,
+	poh.TotalDue,
+	NonRejectedItems = 
+	(
+	SELECT 
+		COUNT(*)
+	FROM Purchasing.PurchaseOrderDetail pod
+	WHERE poh.PurchaseOrderID = pod.PurchaseOrderID
+	AND pod.RejectedQty = 0
+	),
+	MostExpensiveItem =
+	(
+	SELECT 
+		MAX(pod.UnitPrice)
+	FROM Purchasing.PurchaseOrderDetail pod
+	WHERE poh.PurchaseOrderID = pod.PurchaseOrderID
+	)
+FROM Purchasing.PurchaseOrderHeader poh
+
+
+
+
+
